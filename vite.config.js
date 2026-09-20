e0480d84c511fb34cbaeefedcbb494104871ab49
KARTIK  PAWAR <kp346949@gmail.com>
2026-09-20 15:02:44 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // 👈 Works seamlessly with HashRouter on GitHub Pages
+  base: './', 
 })
